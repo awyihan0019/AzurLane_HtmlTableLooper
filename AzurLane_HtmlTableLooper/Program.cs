276db97f0993c8ae1a99bd960ac10d4fc0741d67
yihan <faster960128@hotmail.com>
2017-08-10 14:07:51 +0800

    Fixed image name issue, from 1 to NO.001 -tranform from int to string(Android Studio Must read start char)

--- AzurLane_HtmlTableLooper/Program.cs
+++ AzurLane_HtmlTableLooper/Program.cs
@@ -13,21 +13,19 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            /*
+
             int listNum, NumLines;
             listNum = getNameList();
-
+            Console.WriteLine("Number of data Detected " + listNum);
             for (int i = 0; i < listNum; i++)
             {
                 String nameGet = getHtmlTableDat(listNum, i);
-
                 NumLines = textFilter();
-                generateSQL(NumLines);
+                generateSQL(NumLines, getNo(listNum), i);
+                saveImageFull(listNum, i, getNo(listNum));
                 Console.WriteLine(String.Format("{0,-15} | {1,-15} | {2,-15}", nameGet, "资料已读取", listNum + "/" + (i + 1)));
             }
-            */
-            saveImage();
-
+            saveImage(getNo(listNum));
             Console.WriteLine("\n\n资料已读取完成!!");
             Console.ReadKey();
         }
@@ -87,11 +85,9 @@ namespace ConsoleApp1
                             //writeForFile(data); :for testing output purpose
                             writer.WriteLine(data);
                             //Console.WriteLine(data); :for testing output purpose
-                            Console.WriteLine("已读取数量 ：" + numbersRead);
                             numbersRead++;
                         }
                     }
-
                 }
                 //closs the write file
                 writer.Close();
@@ -222,7 +218,7 @@ namespace ConsoleApp1
                 return false;
         }
 
-        public static void generateSQL(int numLine)
+        public static void generateSQL(int numLine, String[] no, int dataReacher)
         {
             using (StreamWriter sw2 = new StreamWriter(@"C:\Users\yihan\Desktop\database.txt", true))
             {
@@ -231,10 +227,10 @@ namespace ConsoleApp1
                     int line = 0;
                     string[] dataList = new string[numLine];
                     String space;
-                    Console.WriteLine(numLine.ToString());
+                    //Console.WriteLine(numLine.ToString());
                     for (int i = 0; i < numLine; i++)
                     {
-                        if (i != 22 && i != 23 && i != 24 && i != 25 && i != 35 && i != 39 && i != 43 && i != 47 && i != 51)
+                        if (i != 22 && i != 23 && i != 24 && i != 25 && i != 35 && i != 39 && i != 43 && i != 47 && i != 51 && i != 56)
                         {
                             dataList[line] = sr2.ReadLine();
                             line++;
@@ -242,23 +238,22 @@ namespace ConsoleApp1
                         else
                             space = sr2.ReadLine();
                     }
-                    sw2.WriteLine("INSERT INTO KANTAI (name, No, lvl, type, rare, camp, buildTime, dropPoint, value, returnValue, main, sub, hp, amor, filling, atk, tAtk, agi, airDef, airAtk, compsum, speed, lvlAtk, lvlHp, lvlAirDef, lvlAgi, lvlAirAtk, lvlTAtk, star1, star2, star3, usage1, startEquip1, equipType1, usage2, startEquip2, equipType2, usage3, startEquip3, equipType3, usage4, startEquip4, equipType4, usage5, startEquip5, equipType5, skill1, skillEffect1, skill2, skillEffect2, skill3, skillEffect3) VALUES ("
-                        + dataList[0] + "," + dataList[1] + "," + dataList[2] + "," + dataList[3] + "," + dataList[4] + "," + dataList[5] + "," + dataList[6] + "," + dataList[7] + "," + dataList[8] + "," + dataList[9] + ","
-                        + dataList[10] + "," + dataList[11] + "," + dataList[12] + "," + dataList[13] + "," + dataList[14] + "," + dataList[15] + "," + dataList[16] + "," + dataList[17] + "," + dataList[18] + "," + dataList[19] + ","
-                        + dataList[20] + "," + dataList[21] + "," + dataList[22] + "," + dataList[23] + "," + dataList[24] + "," + dataList[25] + "," + dataList[26] + "," + dataList[27] + "," + dataList[28] + "," + dataList[29] + ","
-                        + dataList[30] + "," + dataList[31] + "," + dataList[32] + "," + dataList[33] + "," + dataList[34] + "," + dataList[35] + "," + dataList[36] + "," + dataList[37] + "," + dataList[38] + "," + dataList[39] + ","
-                        + dataList[40] + "," + dataList[41] + "," + dataList[42] + "," + dataList[43] + "," + dataList[44] + "," + dataList[45] + "," + dataList[46] + "," + dataList[47] + "," + dataList[48] + "," + dataList[49] + ","
-                        + dataList[50] + "," + dataList[51] + ");");
+                    sw2.WriteLine("INSERT INTO KANTAI (name, No, lvl, type, rare, camp, buildTime, dropPoint, value, returnValue, main, sub, hp, amor, filling, atk, tAtk, agi, airDef, airAtk, compsum, speed, lvlAtk, lvlHp, lvlAirDef, lvlAgi, lvlAirAtk, lvlTAtk, star1, star2, star3, usage1, startEquip1, equipType1, usage2, startEquip2, equipType2, usage3, startEquip3, equipType3, usage4, startEquip4, equipType4, usage5, startEquip5, equipType5, skill1, skillEffect1, skill2, skillEffect2, skill3, skillEffect3) VALUES (\""
+                        + dataList[0] + "\",\"" + dataList[1] + "\",\"" + dataList[2] + "\",\"" + dataList[3] + "\",\"" + dataList[4] + "\",\"" + dataList[5] + "\",\"" + dataList[6] + "\",\"" + dataList[7] + "\",\"" + dataList[8] + "\",\"" + dataList[9] + "\",\""
+                        + dataList[10] + "\",\"" + dataList[11] + "\",\"" + dataList[12] + "\",\"" + dataList[13] + "\",\"" + dataList[14] + "\",\"" + dataList[15] + "\",\"" + dataList[16] + "\",\"" + dataList[17] + "\",\"" + dataList[18] + "\",\"" + dataList[19] + "\",\""
+                        + dataList[20] + "\",\"" + dataList[21] + "\",\"" + dataList[22] + "\",\"" + dataList[23] + "\",\"" + dataList[24] + "\",\"" + dataList[25] + "\",\"" + dataList[26] + "\",\"" + dataList[27] + "\",\"" + dataList[28] + "\",\"" + dataList[29] + "\",\""
+                        + dataList[30] + "\",\"" + dataList[31] + "\",\"" + dataList[32] + "\",\"" + dataList[33] + "\",\"" + dataList[34] + "\",\"" + dataList[35] + "\",\"" + dataList[36] + "\",\"" + dataList[37] + "\",\"" + dataList[38] + "\",\"" + dataList[39] + "\",\""
+                        + dataList[40] + "\",\"" + dataList[41] + "\",\"" + dataList[42] + "\",\"" + dataList[43] + "\",\"" + dataList[44] + "\",\"" + dataList[45] + "\",\"" + dataList[46] + "\",\"" + dataList[47] + "\",\"" + dataList[48] + "\",\"" + dataList[49] + "\",\""
+                        + dataList[50] + "\",\"" + dataList[51] + "\");");
                 }
             }
         }
 
-        public static void saveImage()
+        public static void saveImage(String[] no)
         {
-            StreamWriter sw2 = new StreamWriter(@"C:\Users\yihan\Desktop\imgURL.txt", true);
+            //StreamWriter sw2 = new StreamWriter(@"C:\Users\yihan\Desktop\imgURL.txt", true);
 
             String url;
-            HtmlNode img;
             String foulder = @"C:\Users\yihan\Desktop\ImageDemo\";
 
             //declare webside
@@ -284,25 +279,74 @@ namespace ConsoleApp1
             for (int i = 0; i < imageNode.Count; i++)
             {
                 url = imageNode[i].GetAttributeValue("src", null);
-                Console.WriteLine(url);
+                //Console.WriteLine(url);
                 var client = new WebClient();
-                client.DownloadFile(url, foulder + (i+1) + ".jpg");
-                Console.WriteLine("Download successfull : " + i+1);
+                client.DownloadFile(url, foulder + no[i] + ".jpg");
+                Console.WriteLine("Download successfull : " + (i + 1));
             }
         }
 
-
-        /*
-        public static void saveImage()
+        public static void saveImageFull(int listNum, int loopTime, String[] no)
         {
             int count = 0;
-            var client = new WebClient();
-            string urlCheck = "";
             String url;
-            HtmlNode img;
-            String foulder = @"C:\Users\yihan\Desktop\ImageDemo\";
-            String name = "";
-            StreamWriter sw2 = new StreamWriter(@"C:\Users\yihan\Desktop\imgURL.txt", true);
+            String foulder = @"C:\Users\yihan\Desktop\ImageFull\";
+
+            var htmlDoc2 = new HtmlDocument();
+
+            String html2;
+            String[] nameList = new String[listNum];
+
+            //set the file to read and write
+            String file_demoFixed = @"C:\Users\yihan\Desktop\demoFixed.txt";
+
+            //
+
+            using (StreamReader sr = new StreamReader(file_demoFixed))
+            {
+                while (sr.Peek() >= 0)
+                {
+                    var strReadLine = sr.ReadLine();
+                    nameList[count] = strReadLine;
+                    count++;
+                }
+            }
+
+            //
+
+            String name = nameList[loopTime];
+            //set website
+            var html = @"http://wiki.joyme.com/blhx/" + name;
+
+            HtmlWeb web2 = new HtmlWeb();
+
+            var htmldoc = web2.Load(html);
+
+            //Line----------------------------------------------------------------Save image below-------------------------------------
+
+            var htmlNodes = htmldoc.DocumentNode.SelectSingleNode("//div[@id='con_1']");
+            //var htmlNodes = htmlDoc.DocumentNode.SelectNodes("//body/h1");
+            //var html = node.OuterHtml();
+            html2 = htmlNodes.InnerHtml;
+
+            htmlDoc2.LoadHtml(html2);
+            HtmlNodeCollection imageNode = htmlDoc2.DocumentNode.SelectNodes("//img");
+            for (int i = 0; i < imageNode.Count; i++)
+            {
+                url = imageNode[i].GetAttributeValue("src", null);
+                //Console.WriteLine(url);
+                var client = new WebClient();
+                client.DownloadFile(url, foulder + no[loopTime] + "_full" + ".jpg");
+                Console.WriteLine("Image download successfull!");
+            }
+        }
+
+        public static String[] getNo(int names)
+        {
+            int count = 0;
+            int listCount = 0;
+            var client = new WebClient();
+            String[] name = new string[names];
 
             //declare webside
             var html = @"http://wiki.joyme.com/blhx/%E6%89%93%E6%8D%9E%E5%88%97%E8%A1%A8";
@@ -311,9 +355,7 @@ namespace ConsoleApp1
             HtmlWeb web = new HtmlWeb();
 
             //load the web
-            var htmldoc = web.Load(html);            
-
-            int numbersRead = 1;
+            var htmldoc = web.Load(html);
 
             //skip firts row
             bool firstRowIsSkipped = false;
@@ -345,34 +387,14 @@ namespace ConsoleApp1
                         String data = cells[k].InnerText;
                         if (k == 0)
                         {
-                            name = data;
-                            Console.WriteLine(data);
-                        }
-                        if (k == 1)
-                        {
-                            img = cells[k].SelectSingleNode("//img");
-                            url = img.GetAttributeValue("src", null);
-                            if (url != urlCheck)
-                            {
-                                Console.WriteLine(url);
-                                urlCheck = url;
-                            }
-                            else
-                                continue;
-                            //sw2.Write(partHTML);
-                            /*
-                              img = row.SelectSingleNode("//img");
-                              url = img.GetAttributeValue("src", null);
-                              Console.WriteLine(url);
-                              client.DownloadFile(url, foulder + numbersRead + ".jpg");
-                              Console.WriteLine("Download successfull : " + numbersRead);
-                              numbersRead++;
-                              
+                            name[listCount] = "NO."+data.Trim();
+                            //Console.WriteLine(data);
+                            listCount++;
                         }
                     }
                 }
             }
-            sw2.Close();
-        }*/
+            return name;
+        }
     }
 }
